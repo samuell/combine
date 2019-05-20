af14b668b9fc1f16f5c6130190157fa9c6e030e6
Samuel Lampa <samuel.lampa@gmail.com>
2019-05-21 00:23:50 +0200

    Improve comments

--- cmd/combine/combine.go
+++ cmd/combine/combine.go
@@ -7,20 +7,23 @@ import (
 )
 
 func main() {
+	// Print usage
 	if len(os.Args) < 2 {
 		fmt.Println("Usage example:\n\n./combine a,b x,y,z 1,2,3,4\n\nAuthor: Samuel Lampa")
 		return
 	}
 
+	// Parse arguments
 	programArgs := os.Args[1:]
-
 	input := make([][]string, len(programArgs))
 	for i, arg := range programArgs {
 		input[i] = strings.Split(arg, ",")
 	}
 
+	// Do the trick
 	result := combine(input)
 
+	// Print some output
 	for _, str := range result {
 		row := strings.Join(str, ", ")
 		fmt.Println(row)
@@ -28,7 +31,6 @@ func main() {
 }
 
 func combine(input [][]string) [][]string {
-
 	// Default case
 	if len(input) <= 1 {
 		return input
@@ -38,19 +40,21 @@ func combine(input [][]string) [][]string {
 	tail := combine(input[1:]) // Recursive call
 
 	result := [][]string{}
-	for _, p := range head {
+	for _, str := range head {
+		// Multiply each string in head with the length of the rows in the tail
+		// (they are guaranteed to be of equal length)
 		for i := 0; i < len(tail[0]); i++ {
 			if len(result) == 0 {
 				result = append(result, []string{})
 			}
-			// Repeat the current part in the current top part of the array
-			result[0] = append(result[0], p)
+			result[0] = append(result[0], str)
 		}
+		// Multiply the content of each row in the tail with the number of rows
+		// in the tail
 		for j := 1; j <= len(tail); j++ {
 			if len(result) <= j {
 				result = append(result, []string{})
 			}
-			// Just pad on the rest
 			result[j] = append(result[j], tail[j-1]...)
 		}
 	}
